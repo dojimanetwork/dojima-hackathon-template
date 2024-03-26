86db8d73d9a5be2358c8ff5322c75c6f5cffb686
Mystical94 <53875398+Mystical94@users.noreply.github.com>
2024-03-26 14:10:16 +0530

    Create MyToken.sol

---
+++ hardhat/contracts/sample_contracts/MyToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+// Replace Your_Name with your name and Token_Symbol with 3 letters form you name
+contract Your_Name is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("Your_Name", "Token_Symbol")
+        Ownable(initialOwner)
+        ERC20Permit("Your_Name")
+    {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
